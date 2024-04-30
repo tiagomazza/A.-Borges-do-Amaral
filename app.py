6d59411fba3b6297d5f5222ceadd6d27f896a89b
tiagomazza <110558042+tiagomazza@users.noreply.github.com>
2024-04-30 11:49:33 +0100

    pins

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from datetime import datetime
 conn = st.connection("gsheets", type=GSheetsConnection)
 
 # Função para escrever o número 1 na planilha do Google Sheets
-def escrever_numero():
+def escrever_numero(nome):
     # Definir o número a ser escrito
     numero = 1
     
@@ -14,24 +14,26 @@ def escrever_numero():
     hora_atual = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
     # Escrever o número na planilha
-    conn.update(
+    conn.append(
         worksheet="Folha",  # Substituir pelo nome da sua planilha
-        data=[{"Número": numero, "Timestamp": hora_atual}]
+        data=[[nome, numero, hora_atual]],
+        start='A1',  # A partir da primeira linha da planilha
+        dimension='ROWS'  # Adiciona como nova linha
     )
 
-    st.success("Número 1 foi escrito na planilha!")
+    st.success(f"Número 1 foi escrito na planilha para {nome}!")
 
 # Interface do Streamlit
 st.title("Aplicativo para Escrever na Planilha")
 
-# Adicionar campo de senha
-senha = st.text_input("Digite a senha:", type="password")
+# Adicionar campo de PIN
+pin_digitado = st.text_input("Digite o seu PIN:")
 
-# Verificar se a senha está correta
-if senha == "senha_correta":
-    st.subheader("Botões disponíveis:")
-    # Botão para escrever o número 1 na planilha quando clicado
-    if st.button("Escrever Número 1 na Planilha"):
-        escrever_numero()
-else:
-    st.warning("Senha incorreta. Por favor, tente novamente.")
+# Verificar se o PIN foi digitado
+if pin_digitado:
+    # Ler os dados da aba "Dados" para encontrar o nome correspondente ao PIN inserido
+    dados = conn.read(worksheet="Dados", usecols=["Pin", "Nome"], ttl=5)
+    
+    # Verificar se o PIN está na lista de PINs válidos
+    if int(pin_digitado) in dados["Pin"].tolist():
+        nome = dados.loc[dados["Pin"] ==
