08d3838976ced4e0a0d8fa4ab22f5a0119b3341e
tiagomazza <110558042+tiagomazza@users.noreply.github.com>
2024-04-30 16:02:36 +0100

    inserçao

--- app.py
+++ app.py
@@ -7,53 +7,25 @@ import pandas as pd
 conn = st.connection("gsheets", type=GSheetsConnection)
 
 # Função para escrever o número 1 na planilha do Google Sheets
-def escrever_numero():
-    # Definir o número a ser escrito
-    numero = 1
-    
+def escrever_registro():
     # Obter a hora atual para registro na planilha
     hora_atual = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
-    # Ler os dados existentes na aba "Folha"
+    # Escrever o número e o timestamp na planilha
     existing_data = conn.read(worksheet="Folha", ttl=5)
     if existing_data is None:
-        existing_data = pd.DataFrame(columns=["Número", "Timestamp"])
+        existing_data = pd.DataFrame(columns=["Nome", "Horário"])
     else:
         existing_data = pd.DataFrame(existing_data)
         
-    # Obter a última linha escrita
-    last_row_index = existing_data.shape[0]
-    
-    # Criar um novo registro com o número e o timestamp
-    new_row = {"Número": numero, "Timestamp": hora_atual}
-    
-    # Adicionar o novo registro na próxima linha abaixo da última linha escrita
-    existing_data = existing_data.append(new_row, ignore_index=True)
-    
-    # Escrever os dados atualizados na planilha
+    existing_data = existing_data.append({"Nome": 1, "Horário": hora_atual}, ignore_index=True)
     conn.update(worksheet="Folha", data=existing_data.to_dict(orient="records"))
 
-    st.success("Número 1 foi escrito na planilha!")
+    st.success("Número e timestamp foram registrados na planilha!")
 
 # Interface do Streamlit
-st.title("Aplicativo para Escrever na Planilha")
+st.title("Aplicativo para Registrar na Planilha")
 
-# Adicionar campo de PIN
-pin_digitado = st.text_input("Digite o seu PIN:", type="password")
-
-# Verificar se o PIN foi digitado
-if pin_digitado:
-    # Ler os dados da aba "Dados" para encontrar o nome correspondente ao PIN inserido
-    dados = conn.read(worksheet="Dados", usecols=["Pin", "Nome"], ttl=5)
-    
-    # Verificar se o PIN está na lista de PINs válidos
-    if int(float(pin_digitado)) in dados["Pin"].tolist():
-        # Dar as boas-vindas utilizando o nome correspondente
-        nome = dados.loc[dados["Pin"] == int(float(pin_digitado)), "Nome"].iloc[0]
-        st.write(f"Bem-vindo, {nome}!")
-        
-        # Botão para escrever o número 1 na planilha quando clicado
-        if st.button("Registrar Número 1 na Planilha"):
-            escrever_numero()
-    else:
-        st.warning("PIN incorreto. Por favor, digite um PIN válido.")
+# Botão para escrever o número 1 na planilha e registrar o timestamp
+if st.button("Registrar na Planilha"):
+    escrever_registro()
