a4f3a11ce62d4131407d9e163ca0387009505445
tiagomazza <110558042+tiagomazza@users.noreply.github.com>
2024-05-08 10:15:53 +0100

    calculo reconv

--- app.py
+++ app.py
@@ -155,9 +155,13 @@ elif pagina_selecionada == "Consultas":
     df = df.groupby(['Data', 'Nome'], as_index=False).agg(lambda x: next(iter(x.dropna()), np.nan))
 
     df['Entrada Manhã conv'] = df['Entrada Manhã'].dt.hour * 60 + df['Entrada Manhã'].dt.minute
+<<<<<<< HEAD
+    # Criar coluna 'Entrada Manhã reconv' com valores reconvertidos para formato hh:mm
+    df['Entrada Manhã reconv'] = pd.to_datetime(df['Entrada Manhã conv'], unit='m').dt.strftime('%H:%M')
     df['Saída Manhã conv'] = df['Saída Manhã'].dt.hour * 60 + df['Saída Manhã'].dt.minute
-    df['Total trabalhado'] = df['Saída Manhã conv'] - df['Entrada Manhã conv']
-    df['Total trabalhado'] = pd.to_datetime(df['Total trabalhado'], unit='m').dt.strftime('%H:%M')
+    df['Total trabalhado'] = df['Saída Manhã conv']- df['Entrada Manhã conv']
+    df['Total trabalhado reconv'] = pd.to_datetime(df['Total trabalhado'], unit='m').dt.strftime('%H:%M')
+
 
     # Exibir o DataFrame na página
     st.write(df)
