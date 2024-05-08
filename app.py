9cd13d583e2d416e312e57dc04214388629446d2
tiagomazza <110558042+tiagomazza@users.noreply.github.com>
2024-05-08 10:02:24 +0100

    conv

--- app.py
+++ app.py
@@ -153,7 +153,7 @@ elif pagina_selecionada == "Consultas":
 
     # Agrupar linhas com mesma Data e Nome
     df = df.groupby(['Data', 'Nome'], as_index=False).agg(lambda x: next(iter(x.dropna()), np.nan))
-    df['Entrada Manhã conv'] = "2"
+    df['Entrada Manhã conv'] = df['Entrada Manhã'].dt.hour * 60 + df['Entrada Manhã'].dt.minute
     df['Total trabalhado'] = "1"
     # Exibir o DataFrame na página
     st.write(df)
