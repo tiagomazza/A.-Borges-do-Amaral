58a373c560fb5d8877fe93bc3bbb26ab1504be3b
tiagomazza <110558042+tiagomazza@users.noreply.github.com>
2024-05-08 11:46:52 +0100

    saida manha

--- app.py
+++ app.py
@@ -143,11 +143,11 @@ elif pagina_selecionada == "Consultas":
     df = pd.DataFrame(data)
     df['Entrada Manhã'] = pd.to_datetime(df['Entrada Manhã'])
     df['Entrada Manhã em numeros'] = df['Entrada Manhã'].dt.hour * 60 + df['Entrada Manhã'].dt.minute
-    df['Entrada Manhã (Minutos)'] = df['Entrada Manhã em numeros']
+    #df['Entrada Manhã (Minutos)'] = df['Entrada Manhã em numeros']
     
     df['Saída Manhã'] = pd.to_datetime(df['Saída Manhã'])
     df['Saída Manhã em numeros'] = df['Saída Manhã'].dt.hour * 60 + df['Entrada Manhã'].dt.minute
-    df['Saída Manhã (Minutos)'] = df['Saída Manhã em numeros']
+    #df['Saída Manhã (Minutos)'] = df['Saída Manhã em numeros']
 
     df['Entrada Tarde'] = pd.to_datetime(df['Entrada Tarde'])
     df['Entrada Tarde em numeros'] = df['Entrada Tarde'].dt.hour * 60 + df['Entrada Manhã'].dt.minute
