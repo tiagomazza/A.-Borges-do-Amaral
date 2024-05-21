960154ff2e3f86c12c5f30547102714e19dccdc9
tiagomazza <110558042+tiagomazza@users.noreply.github.com>
2024-05-21 16:07:52 +0100

    verifique

--- app.py
+++ app.py
@@ -313,4 +313,4 @@ if st.sidebar.text_input("",type="password") == str(senha_admin):
         if st.button("Salvar dados"):
             save_to_new_sheet(grouped_data)
 else:
-    pass
+    st.sidebar.warning("Verifique o PIN")
