a26f13b714f7f7c6be60caa9e49d78f2ba35035f
tiagomazza <110558042+tiagomazza@users.noreply.github.com>
2024-06-14 11:53:02 +0100

    int

--- app.py
+++ app.py
@@ -173,7 +173,7 @@ if pagina_selecionada == "✍🏽Marcação de Ponto":
             st.warning("Utilize somente numeros")
 
 # Página inicial para entrada da senha
-if st.sidebar.text_input int("",type="password") == (senha_admin):
+if st.sidebar.text_input (int("",type="password")) == (senha_admin):
     # Conteúdo das abas "Consultas" e "Admin" aqui
 
     if pagina_selecionada == "🔍Consultas":
