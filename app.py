3e38ff9c9fdff4731d88e12a0d943d793bdb869d
tiagomazza <110558042+tiagomazza@users.noreply.github.com>
2025-04-16 15:19:19 +0100

    update morning entry +2

--- app.py
+++ app.py
@@ -92,7 +92,7 @@ if pagina_selecionada == "✍🏽Marcação de Ponto":
 
                 if st.button("☕ Entrada Manhã"):
                                 current_time = datetime.now()
-                                one_hour_after = current_time + timedelta(hours=1)
+                                one_hour_after = current_time + timedelta(hours=2)
                                 submission_datetime = current_time.strftime("%Y-%m-%d %H:%M:%S") #one_hour_after.strftime("%Y-%m-%d %H:%M:%S")
                                 new_data = pd.DataFrame({
                                     "Name": [nome],
